c8526bdd5b85073bb1fd4245e75e9cfb8fd74826
MuraliJbk <murali@jbkinfotech.com>
2024-04-12 17:19:29 +0530

    tets

---
+++ js/modules/custom-slick.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($){
+    $(".asset-scroll-slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        arrows: false,
+        swipeToSlide: true,
+        // autoplay: true,
+        // autoplaySpeed: 0,
+        speed: 6000,
+        infinite: true,
+        cssEase: 'linear'
+    });
+
+    
+       
+});
