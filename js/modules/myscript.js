a07893df2729e8991785183bf4408f3fd673a994
MuraliJbk <murali@jbkinfotech.com>
2024-04-13 12:55:16 +0530

    tets

---
+++ js/modules/myscript.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($){
+    $('.accordion-header').on('click', function(e){
+        e.preventDefault();
+        $(this).parent().toggleClass('active');
+        $(this).parent().siblings().removeClass('active');
+        $(this).parent().siblings().find('.accordion-header').removeClass('open');
+        $(this).toggleClass("open");
+        $(this).siblings('.accordion-content').slideToggle(500);
+        $(this).parent().siblings().find('.accordion-content').slideUp(500);
+    });
+});
