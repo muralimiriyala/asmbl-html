b4521cfa2846d5a1a494070fad2bcbbc74b5a1f1
MuraliJbk <murali@jbkinfotech.com>
2024-04-14 16:44:32 +0530

    paths

--- js/modules/myscript.js
+++ js/modules/myscript.js
@@ -1,4 +1,21 @@
 jQuery(document).ready(function($){
+
+    $(".humburger-btn").on("click", function(e){
+        e.preventDefault();
+        $(this).toggleClass("open");
+    });
+
+    if($(window).width() <= 1023){
+        let level1 = $("ul.main_menu > li.menu-item-has-children > a");
+        level1.on("click", function(e){
+            e.preventDefault();
+            $(this).parent("li").siblings().children("a").removeClass("active");
+            $(this).toggleClass("active");
+            $(this).parent("li").siblings().children("ul").slideUp(900);
+            $(this).siblings("ul").slideToggle(900);
+        });
+    }
+
     $('.accordion-header').on('click', function(e){
         e.preventDefault();
         $(this).parent().toggleClass('active');
@@ -7,5 +24,11 @@ jQuery(document).ready(function($){
         $(this).toggleClass("open");
         $(this).siblings('.accordion-content').slideToggle(500);
         $(this).parent().siblings().find('.accordion-content').slideUp(500);
+    });
+
+    $(".faqs-category-btn").on('click', function(e){
+        e.preventDefault();
+        $(this).toggleClass("active");
+        $("ul.faqs-links").slideToggle(900)
     });
 });
