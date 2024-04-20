d6f2eda676ffdc945332fe468f009cc4f1e55291
MuraliJbk <murali@jbkinfotech.com>
2024-04-20 16:14:16 +0530

    paths

--- js/modules/sidebar-sticky.js
+++ js/modules/sidebar-sticky.js
@@ -1,52 +1,26 @@
-
-// document.addEventListener("DOMContentLoaded", function(){
-//     const header = document.querySelector("header.site-header");
-//     const hHeight = header.clientHeight;
-//     const stickySidebar = document.querySelector(".sticky-sidebar");
-//     const stickySidebartop = document.querySelector(".sticky-sidebar-top");
-//     const stickyOffset = stickySidebartop.offsetTop;
-//     const stickyRemove = document.querySelector(".sticky-content");
-//     const stickyHeight = stickyRemove.clientHeight;    
-//     window.onscroll = function(){
-//         let scroll = window.scrollY;
-//         console.log(scroll, stickyOffset)
-//         if (scroll >= hHeight && scroll <= stickyHeight + stickyOffset - stickySidebar.clientHeight) {
-//             stickySidebar.classList.add("sticky");
-//         } else {
-//             stickySidebar.classList.remove("sticky");
-//         }
-//     }
-// });
-
-
-
-
 document.addEventListener("DOMContentLoaded", function(){
     const header = document.querySelector("header.site-header");
     const hHeight = header.clientHeight;
     const stickySidebar = document.querySelector(".sticky-sidebar");
-    const stickySidebartop = document.querySelector(".sticky-sidebar-top");
+    const stickySidebartop = document.querySelector(".aside-sticky-sidebar");
     const stickyOffset = stickySidebartop.offsetTop;
-    const stickyRemove = document.querySelector(".sticky-content");
-    const stickyHeight = stickyRemove.clientHeight;
-    
-    const footerOffset = document.querySelector(".site-footer").offsetTop;
+
+    const footer = document.querySelector(".site-footer");
+    const footerOffset = footer.offsetTop;
     const sidebarHeight = stickySidebar.offsetHeight;
     const maxY = footerOffset - sidebarHeight - stickyOffset;
+    const threshold = stickyOffset - hHeight; // Distance from top to trigger "sticky" behavior
 
     window.onscroll = function(){
         let scroll = window.scrollY;
-
-        if (scroll > stickyOffset) {
-            if (scroll < maxY) {
-                stickySidebar.classList.add("sticky");
-                stickySidebar.style.position = 'fixed';
-                stickySidebar.style.top = '0';
-            } else {
-                stickySidebar.classList.remove("sticky");
-                stickySidebar.style.position = 'absolute';
-                stickySidebar.style.top = (maxY - stickyOffset) + 'px';
-            }
+        if (scroll >= threshold && scroll <= maxY + threshold) {
+            stickySidebar.classList.add("sticky");
+            stickySidebar.style.position = 'fixed';
+            stickySidebar.style.top = hHeight + 'px'; // Adjusting top position by header height
+        } else if (scroll > maxY + threshold) {
+            stickySidebar.classList.remove("sticky");
+            stickySidebar.style.position = 'absolute';
+            stickySidebar.style.top = (footerOffset - (sidebarHeight + stickyOffset)) + 'px'; // Adjusting top position for footer
         } else {
             stickySidebar.classList.remove("sticky");
             stickySidebar.style.position = 'relative';
