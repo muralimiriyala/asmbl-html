8cf37f5b2c1f71e6b99c9db3934023ffe210ad7b
MuraliJbk <murali@jbkinfotech.com>
2024-04-25 21:02:03 +0530

    paths

--- js/modules/custom-slick.js
+++ js/modules/custom-slick.js
@@ -10,8 +10,21 @@ jQuery(document).ready(function($){
         speed: 6000,
         infinite: true,
         cssEase: 'linear',
-        
     });
+
+    $(".client-slider-1").slick({
+        slidesPerRow: 2,
+		slidesToShow: 2,
+        vertical: true,
+        verticalSwiping: true,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 6000,
+        arrows: false,
+        focusOnSelect: true,
+        // dots: false,
+    });
+    
 
     
        
