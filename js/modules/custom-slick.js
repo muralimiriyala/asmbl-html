c7cf14871902a7b6c2f277bb405a375e961b7a65
MuraliJbk <murali@jbkinfotech.com>
2024-05-02 21:02:27 +0530

    test

--- js/modules/custom-slick.js
+++ js/modules/custom-slick.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function($){
-    var _windowWidth = $(window).width();
     $(".client-slider-1").slick({
         slidesPerRow: 2,
 		slidesToShow: 2,
@@ -13,54 +12,48 @@ jQuery(document).ready(function($){
         // dots: false,
     });
 
-    function nameSlider(){
-    const pageSlider = $(".industry-slider");
-    
-const pageSlide = pageSlider.children(".industry-slide").length;
-const isMobile = _windowWidth <= 1024;
-
-if (!isMobile && pageSlide <= 4) {
-    if (pageSlider.hasClass('slick-initialized')) {
-        pageSlider.slick('unslick');
-    }
-} else if (!isMobile && pageSlide >= 4) {
-    pageSlider.slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        arrows: true,
-        prevArrow: '<div class="slick-arrow slick-prev flex flex-center"><span class="slick-arrows slick-prev-arrow"></span></div>',
-        nextArrow: '<div class="slick-arrow slick-next flex flex-center"><span class="slick-arrows slick-next-arrow"></span></div>',  
-        dots: false,
-        speed: 1500,
-        infinite: false,
-        autoplay: false,
-        variableWidth: true,
-    });
-} else if (isMobile && pageSlide >= 4) {
-    pageSlider.slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        prevArrow: '<div class="slick-arrow slick-prev flex flex-center"><span class="slick-arrows slick-prev-arrow"></span></div>',
-        nextArrow: '<div class="slick-arrow slick-next flex flex-center"><span class="slick-arrows slick-next-arrow"></span></div>',  
-        dots: false,
-        speed: 1500,
-        infinite: false,
-        autoplay: false,
-        variableWidth: true,
-    });
-}
-    }
-    nameSlider();
-
-$(window).on('resize load', function () {
-    var newScreenWidth = $(window).width();
-    if (newScreenWidth !== _windowWidth) {
-      _windowWidth = newScreenWidth;
-      nameSlider();
-  
-    }
-  });
-
-
 });
+
+const pageSlider = $(".industry-slider");
+const pageSlide = pageSlider.children(".industry-slide").length;
+let mobileMedia = function(){
+    if(jQuery(window).width() <= 1023){   
+        pageSlider.slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            prevArrow: '<div class="slick-arrow slick-prev flex flex-center"><span class="slick-arrows slick-prev-arrow"></span></div>',
+            nextArrow: '<div class="slick-arrow slick-next flex flex-center"><span class="slick-arrows slick-next-arrow"></span></div>',
+            dots: false,
+            speed: 1500,
+            infinite: false,
+            autoplay: false,
+            variableWidth: true,
+        });
+    }
+}
+jQuery(window).resize(function () { mobileMedia(); });
+jQuery(document).on('ready load', function () { mobileMedia(); });
+let desktop = function(){
+    if(jQuery(window).width() >= 1024){ 
+        if (pageSlide <= 4) {
+            pageSlider.slick('unslick');
+        }
+        else{
+            pageSlider.slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: true,
+                prevArrow: '<div class="slick-arrow slick-prev flex flex-center"><span class="slick-arrows slick-prev-arrow"></span></div>',
+                nextArrow: '<div class="slick-arrow slick-next flex flex-center"><span class="slick-arrows slick-next-arrow"></span></div>',
+                dots: false,
+                speed: 1500,
+                infinite: false,
+                autoplay: false,
+                variableWidth: true,
+            }); 
+        }
+    }
+}   
+jQuery(window).resize(function () { desktop(); });
+jQuery(document).on('ready load', function () { desktop(); });
