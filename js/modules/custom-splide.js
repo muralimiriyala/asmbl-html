2c0f57f6301b3d9625396e2e6585f60a62257df4
MuraliJbk <murali@jbkinfotech.com>
2024-05-05 22:22:33 +0530

    test

--- js/modules/custom-splide.js
+++ js/modules/custom-splide.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function(){
     const test = document.querySelector(".splide");
     if(test !== null){ 
         new Splide(test, {
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
             drag: "free",
             focus: "left",
             perPage: 6,
+            fixedWidth: "256px",
             autoScroll: {
                 speed: 1.5
             },
