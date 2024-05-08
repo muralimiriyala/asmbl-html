0fd3a76212bee808594b31f0b4cfb335cdf3dfd2
MuraliJbk <murali@jbkinfotech.com>
2024-05-08 13:39:00 +0530

    test

--- js/modules/custom-splide.js
+++ js/modules/custom-splide.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const mysplide = document.querySelector(".splide");
-    if(mysplide !== null){ 
-        new Splide(mysplide, {
+const Splider = jQuery(".asset-scroll-slider");
+let mobileSplide = function () {
+    if (jQuery(window).width() <= 1299) {
+        new Splide(Splider[0], { // Splider[0] to get the DOM element
             type: "loop",
             drag: "free",
             focus: "left",
@@ -14,5 +14,33 @@ document.addEventListener("DOMContentLoaded", function(){
             pagination: false
         }).mount(window.splide.Extensions);
     }
-});
+}
+jQuery(document).on('ready load', function () { mobileSplide(); });
 
+let desktopSplide = function () {
+    if (jQuery(window).width() >= 1300) {
+        const pageSplide = Splider.find('.splide__slide').length;
+        console.log(pageSplide)
+        if (pageSplide <= 4) {
+
+            new Splide(Splider[0], { // Splider[0] to get the DOM element
+                destroy: true,
+            }).mount(window.splide.Extensions);
+            
+        } else {
+            new Splide(Splider[0], { // Splider[0] to get the DOM element
+                type: "loop",
+                drag: "free",
+                focus: "left",
+                perPage: 1,
+                fixedWidth: "256px",
+                autoScroll: {
+                    speed: 1.5
+                },
+                arrows: false,
+                pagination: false
+            }).mount(window.splide.Extensions);
+        }
+    }
+}
+jQuery(document).on('ready load', function () { desktopSplide(); });
