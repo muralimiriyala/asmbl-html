b1766122dd65961e7c1c4159a92b2c10ced29c53
MuraliJbk <murali@jbkinfotech.com>
2024-05-12 12:16:25 +0530

    test

--- js/modules/myscript.js
+++ js/modules/myscript.js
@@ -9,16 +9,6 @@ jQuery(document).ready(function($){
         $(this).parent().siblings().find('.accordion-content').slideUp(500);
     });
 
-    $(".prod-art-title").on("click", function(e){
-        e.preventDefault();
-        $(this).parent().toggleClass('active');
-        $(this).parent().siblings().removeClass('active');
-        $(this).parent().siblings().find('.prod-art-title').removeClass('open');
-        $(this).toggleClass("open");
-        $(this).siblings('.prod-art-content').slideToggle(500);
-        $(this).parent().siblings().find('.prod-art-content').slideUp(500);
-    });
-
     $(".faqs-category-btn").on('click', function(e){
         e.preventDefault();
         $(this).toggleClass("active");
--- js/modules/prod-overview.js
+++ js/modules/prod-overview.js
@@ -1,4 +1,27 @@
 jQuery(document).ready(function(){
+
+    jQuery('.prod-overview-section').each(function(index){
+        const $prodSlide = jQuery(this);
+        jQuery(".prod-overview-bg:first-child").addClass("active");
+        jQuery(".prod-overview-bg:first-child .prod-art-head").addClass("open");
+        jQuery(".prod-overview-bg:first-child .prod-art-content").slideDown(500);
+        jQuery('.prod-image-list:first-child').addClass('active-slide');
+
+        $prodSlide.find(".prod-art-head").on("click", function(e){
+            e.preventDefault();
+            jQuery(this).parent().toggleClass('active');
+            jQuery(this).parent().siblings().removeClass('active');
+            jQuery(this).parent().siblings().find('.prod-art-head').removeClass('open');
+            jQuery(this).toggleClass("open");
+            jQuery(this).siblings('.prod-art-content').slideToggle(500);
+            jQuery(this).parent().siblings().find('.prod-art-content').slideUp(500);
+
+            /*-- slide --*/
+            let data = jQuery(this).data("name");
+            $prodSlide.find('.prod-image-list').removeClass('active-slide');
+            $prodSlide.find('.prod-image-list[data-image="' + data + '"]').addClass('active-slide');
+        });
+    });
     jQuery('.prod-overview-section').each(function(index){
         var $container = jQuery(this);
         var $imageSlider = $container.find('.prod-image-slider');
