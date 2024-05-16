55ac43df3f11daeca4700f13621b4ad73c67310f
MuraliJbk <murali@jbkinfotech.com>
2024-05-16 20:38:43 +0530

    14-05-2024

--- js/modules/resources.js
+++ js/modules/resources.js
@@ -1,12 +1,5 @@
 
 jQuery(document).ready(function(){
-
-    // $("button.res-srch-icon").on('click', function(e){
-    //     e.preventDefault();
-    //     $(".res-dropdown-main").toggleClass('fade');
-    //     $(".res-srch-form").toggleClass("open");
-    // });
-
     jQuery(".res-dropdown").on('click', function(e){
         e.preventDefault();
         jQuery(this).parent().siblings(".res-dropdown-pos").find(".res-dropdown").removeClass("open");
@@ -14,14 +7,7 @@ jQuery(document).ready(function(){
         jQuery(this).parent().siblings(".res-dropdown-pos").find(".res-tags-list").fadeOut(500);
         jQuery(this).siblings(".res-tags-main").find(".res-tags-list").fadeToggle(500);
     });
-    // $("body").on("click", function(e){
-    //     if(!$(e.target).closest("button.res-srch-icon, .res-srch-form, res-dropdown-main").length){
-    //         $(".res-dropdown-main").removeClass('fade');
-    //         $(".res-srch-form").removeClass("open");
-    //     }
-    // });
 });
-
 function srchResize(){
     if (window.matchMedia('(min-width: 768px)').matches) {
         jQuery("button.res-srch-icon").on('click', function(e){
