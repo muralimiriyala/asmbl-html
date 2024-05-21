56f6098984bbaed84d6f759bdfd43d87a4eccddd
MuraliJbk <murali@jbkinfotech.com>
2024-05-21 21:11:40 +0530

    14-05-2024

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -36,6 +36,21 @@ function menu(){
         });
     }
 }
-jQuery(document).on("ready", function() { menu(); });
-
-  
+jQuery(document).on("ready load", function() { menu(); });
+function desktopMenu(){
+    if(window.matchMedia('(min-width: 936px)').matches){
+        jQuery(".humburger-btn").removeClass("open");
+        jQuery(".header_right").removeAttr("style");
+        jQuery(".h_mobile_overlay").removeClass("open");
+    }
+}
+jQuery(document).on("ready", function() { desktopMenu(); });
+jQuery(window).on("resize", function() { desktopMenu(); });
+const observer = new MutationObserver(function(mutationsList) {
+    for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            desktopMenu();
+        }
+    }
+});
+observer.observe(document.body, { childList: true, subtree: true });
